b8d87dd226781abf66762140da9ce0cc37411e78
omaralmhammed <almahammedomar0.com>
2024-10-22 21:47:49 +0300

    set up database base connection string and create the book model

---
+++ DESAISIV-Task-Omar-Almahammed/models/Book.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DESAISIV_Task_Omar_Almahammed.models
+{
+    public class Book
+    {
+        [Key]
+        int Id { get; set; }
+        string Title { get; set; }
+        string Author { get; set; }
+        int PublicationYear { get; set; }
+    }
+}
---
+++ DESAISIV-Task-Omar-Almahammed/models/MyDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace DESAISIV_Task_Omar_Almahammed.models
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+
+        }
+
+
+    }
+}
