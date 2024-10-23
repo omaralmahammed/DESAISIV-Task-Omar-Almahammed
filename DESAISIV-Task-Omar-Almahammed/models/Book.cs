b2e5d4b836b731d5e4b0451c74a3e9cf9901493e
omaralmhammed <almahammedomar0.com>
2024-10-23 20:27:07 +0300

    delete weather controller and add message erorr on table validation

--- DESAISIV-Task-Omar-Almahammed/models/Book.cs
+++ DESAISIV-Task-Omar-Almahammed/models/Book.cs
@@ -6,8 +6,17 @@ namespace DESAISIV_Task_Omar_Almahammed.models
     {
         [Key]
         public int BookId { get; set; }
+
+        [Required]
+        [MinLength(1, ErrorMessage = "Please enter a valid title name.")]
+
         public string Title { get; set; }
+
+        [Required]
+        [MinLength(1, ErrorMessage = "Please enter a valid author name.")]
         public string Author { get; set; }
+
+        [Range(1800, 2024, ErrorMessage = "Publication year must be between 1900 and 2024.")]
         public int PublicationYear { get; set; }
     }
 }
